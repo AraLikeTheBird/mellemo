e7dafa0b2bd692893d1e30140ca79dd09040c430
ara <arw001@edu.zealand.dk>
2026-09-09 10:22:33 +0200

    added quick scroll button- needs changing of placeholders and text

--- jscode.js
+++ jscode.js
@@ -109,3 +109,32 @@ document.querySelectorAll(".slider").forEach(slider => {
 });
 
 
+
+
+//quick scroll
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+// scroll to top on click
+scrollBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+
